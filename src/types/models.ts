c065582e186d4a026837fec0b45df1bb29873927
kmhdesign123 <kmhenson.design@gmail.com>
2023-06-07 14:04:15 -0500

    create new Vote interface in models types

--- src/types/models.ts
+++ src/types/models.ts
@@ -4,12 +4,22 @@
 
 /* ---------===== auth models =====--------- */
 
+export interface Vote {
+  id: number;
+  value: number;
+  profileId: number;
+  voterId: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface Profile {
   name: string;
   photo?: string;
   id: number;
   createdAt: string;
   updatedAt: string;
+  votesReceived: Vote[];
 }
 
 export interface User {
