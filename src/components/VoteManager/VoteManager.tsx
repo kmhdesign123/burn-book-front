dbe026f727c11393358c588155052c4abbb09b36
kmhdesign123 <kmhenson.design@gmail.com>
2023-06-08 13:17:43 -0500

    add hover state functionality

--- src/components/VoteManager/VoteManager.tsx
+++ src/components/VoteManager/VoteManager.tsx
@@ -1,3 +1,6 @@
+// npm modules
+import { useState } from 'react'
+
 // assets
 import lipstick from '../../assets/icons/lipstick_print.png'
 import noLipstick from '../../assets/icons/no_lipstick_print.png'
@@ -14,6 +17,8 @@ interface VoteManagerProps {
 const VoteManager = (props: VoteManagerProps): JSX.Element => {
 	const { profile, handleVote } = props
 
+  const [hover, setHover] = useState<string | null>(null)
+
   const ratingOptions = [ 1, 2, 3, 4, 5 ]
   const voteCount = profile.votesReceived.length
   let voteSum = 0
@@ -26,6 +31,13 @@ const VoteManager = (props: VoteManagerProps): JSX.Element => {
     const newValue = parseInt(evt.currentTarget.id)
     handleVote({ value: newValue, profileId: profile.id })
   }
+  const handleHover = (evt: React.MouseEvent): void => {
+    if (evt.type === 'mouseover') {
+      setHover(evt.currentTarget.id)
+    } else if (evt.type === 'mouseleave') {
+      setHover(null)
+    }
+  }
 
   return (
     <section>
@@ -34,6 +46,8 @@ const VoteManager = (props: VoteManagerProps): JSX.Element => {
           id={rating.toString()}
           key={rating}
           onClick={handleClick}
+          onMouseOver={handleHover}
+          onMouseLeave={handleHover}
           src={rating <= profileRating ? lipstick : noLipstick}
 					alt="Lipstick Print"
         />
